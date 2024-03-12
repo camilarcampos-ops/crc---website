9a653c2523599a4769fa36484e2b4eed75621901
camilarcampos <122387526+camilarcampos-ops@users.noreply.github.com>
2024-03-11 19:11:47 -0700

    new changes, add more info into the portfolio page

--- src/index.js
+++ src/index.js
@@ -13,3 +13,35 @@ new Typewriter("#titleHero", {
   cursor: null,
   delay: 50,
 });
+
+function initAnimation(id, animationClass) {
+  const minWidthForAnimation = 768;
+
+  if (window.innerWidth >= minWidthForAnimation) {
+    const element = document.querySelector(id);
+
+    if (element) {
+      const observer = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add(animationClass);
+              observer.unobserve(entry.target);
+            }
+          });
+        },
+        {
+          rootMargin: "0px",
+          threshold: 0.1,
+        }
+      );
+
+      observer.observe(element);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initAnimation("#slideLeft", "slide-in-left");
+  initAnimation("#slideRight", "slide-in-right");
+});

58eaf0ffab02f978be9382d635566d1be3c26575
camilarcampos <122387526+camilarcampos-ops@users.noreply.github.com>
2024-03-14 13:38:07 -0700

    fix bug in the portfolio page

--- src/index.js
+++ src/index.js
@@ -45,24 +45,3 @@ document.addEventListener("DOMContentLoaded", function () {
   initAnimation("#slideLeft", "slide-in-left");
   initAnimation("#slideRight", "slide-in-right");
 });
-
-// portfolio
-$(".gallery ul li a").click(function () {
-  var itemID = $(this).attr("href");
-  $(".gallery ul").addClass("item_open");
-  $(itemID).addClass("item_open");
-  return false;
-});
-$(".close").click(function () {
-  $(".port, .gallery ul").removeClass("item_open");
-  return false;
-});
-
-$(".gallery ul li a").click(function () {
-  $("html, body").animate(
-    {
-      scrollTop: parseInt($("#top").offset().top),
-    },
-    600
-  );
-});
